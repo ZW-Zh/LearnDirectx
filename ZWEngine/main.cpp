820e9c932677bd0394261af38ad8418a45d7b77e
zevzhang <zevzhang8320@gmail.com>
2022-10-11 23:21:23 +0800

    update color

--- ZWEngine/main.cpp
+++ ZWEngine/main.cpp
@@ -1,7 +1,10 @@
 #include "stdafx.h"
+using namespace DirectX;
 
 struct Vertex {
-    DirectX::XMFLOAT3 pos;
+    Vertex(float x, float y, float z, float r, float g, float b, float a) : pos(x, y, z), color(r, g, b, z) {}
+    XMFLOAT3 pos;
+    XMFLOAT4 color;
 };
 
 int WINAPI WinMain(HINSTANCE hInstance,    //Main windows function
@@ -441,7 +444,8 @@ bool InitD3D()
 
     D3D12_INPUT_ELEMENT_DESC inputLayout[] =
     {
-        { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
+        { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+        { "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
     };
 
     // fill out an input layout description structure
@@ -487,9 +491,9 @@ bool InitD3D()
 
     // a triangle
     Vertex vList[] = {
-        { { 0.0f, 0.5f, 0.5f } },
-        { { 0.5f, -0.5f, 0.5f } },
-        { { -0.5f, -0.5f, 0.5f } },
+     { 0.0f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f },
+     { 0.5f, -0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f },
+     { -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f },
     };
 
     int vBufferSize = sizeof(vList);
